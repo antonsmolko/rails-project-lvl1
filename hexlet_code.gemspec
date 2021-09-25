96efad3e2eb896f22ec9516d4dd05ba53e925175
Anton Smolko <asmolko@yandex.ru>
2021-09-25 23:09:38 +0300

    step 3: add rubocop linter for project and github actions

--- hexlet_code.gemspec
+++ hexlet_code.gemspec
@@ -12,7 +12,7 @@ Gem::Specification.new do |spec|
   spec.description   = "Form generator - DSL, with the help of which it is convenient to generate forms"
   spec.homepage      = "https://github.com/antonsmolko/rails-project-lvl1"
   spec.license       = "MIT"
-  spec.required_ruby_version = ">= 2.4.0"
+  spec.required_ruby_version = ">= 3.0.0"
 
   spec.metadata["allowed_push_host"] = "https://github.com/antonsmolko/rails-project-lvl1"
 
--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,17 +2,20 @@
 
 require_relative "hexlet_code/version"
 
+# HexletCode module
 module HexletCode
   class Error < StandardError; end
 
+  # Tag module returns html string for tag
   module Tag
-    def self.build(name, attributes = {})
-      attrs = attributes.reduce('') { |memo, (key, value)| %(#{memo} #{key}="#{value}")}
+    def self.build(tag_name, attributes = {})
+      attr_s = attributes.reduce("") { |memo, (key, value)| %(#{memo} #{key}="#{value}") }
       single_template = ->(name, attrs) { "<#{name}#{attrs}/>" }
       pair_template = ->(name, attrs, body) { "<#{name}#{attrs}>#{body}</#{name}>" }
 
-      return pair_template.call(name, attrs, yield) if block_given?
-      single_template.call(name, attrs)
+      return pair_template.call(tag_name, attr_s, yield) if block_given?
+
+      single_template.call(tag_name, attr_s)
     end
   end
 end
