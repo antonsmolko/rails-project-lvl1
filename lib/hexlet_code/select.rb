d36aea87aba14b4d401ebd75f0904e7b1de5e50e
Anton Smolko <asmolko@yandex.ru>
2021-10-04 23:24:45 +0300

    add second fixture

--- lib/hexlet_code/select.rb
+++ lib/hexlet_code/select.rb
@@ -8,10 +8,11 @@ module HexletCode
     def self.build(name, value: nil, **attrs)
       collection = attrs.delete(:collection) || []
       attrs_h = { name: name, **attrs }
+      is_multiple = attrs.key?(:multiple) && attrs[:multiple] == true
 
       options = collection.map do |option|
         option_attrs = { value: option }
-        option_attrs[:selected] = true if option == value
+        option_attrs[:selected] = true if is_multiple ? value.include?(option) : option == value
         option_tag = Tag.build('option', **option_attrs) { option }
         %(\n#{option_tag})
       end.join
--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -7,11 +7,12 @@ class HexletCodeTest < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
-  User = Struct.new :name, :job, :gender, keyword_init: true
+  User1 = Struct.new :name, :job, :gender, keyword_init: true
+  User2 = Struct.new :name, :age, :job, :gender, :citizenship, keyword_init: true
 
-  def test_form_generator
-    user = User.new name: 'rob', job: 'hexlet', gender: 'm'
-    expected = read_fixture_file('expected.html')
+  def test_simple_form
+    user = User1.new name: 'rob', job: 'hexlet', gender: 'm'
+    expected = read_fixture_file('expected_1.html')
 
     form = HexletCode.form_for user do |f|
       f.input :name
@@ -20,6 +21,19 @@ class HexletCodeTest < Minitest::Test
       f.submit
     end
 
+    assert_equal expected, form
+  end
+
+  def test_complex_form
+    user = User2.new name: 'vika', age: 39, job: 'google', citizenship: %w[russia england]
+    expected = read_fixture_file('expected_2.html')
+    form = HexletCode.form_for user do |f|
+      f.input :name
+      f.input :age
+      f.input :job, as: :text
+      f.input :citizenship, as: :select, multiple: true, collection: %w[russia germany england usa]
+      f.submit
+    end
     assert_equal expected, form
   end
 end
